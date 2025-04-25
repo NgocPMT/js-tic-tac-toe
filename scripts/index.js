57e016c8381edbf9da6b6b6161bb0140ab0d573f
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-04-25 22:13:57 +0700

    remove isNext property of player object
    change the scope of isNext from player to gameController for better control of the game

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-  let gameBoard = Array(9).fill("0");
+  let gameBoard = Array(9).fill("-1");
 
   const getBoard = () => gameBoard;
 
@@ -8,36 +8,28 @@ const gameBoard = (function () {
   return { getBoard, setBoard };
 })();
 
-function createPlayer(name, marker, isNext) {
-  const switchTurn = () => (isNext = !isNext);
-
-  return { name, marker, isNext, switchTurn };
+function createPlayer(name, marker) {
+  return { name, marker };
 }
 
 const gameController = (function () {
-  const player1 = createPlayer("Player 1", "X", true);
-  const player2 = createPlayer("Player 2", "O", false);
+  const player1 = createPlayer("Player 1", "X");
+  const player2 = createPlayer("Player 2", "O");
+  let xIsNext = true;
 
   const renderBoard = () => {
     const board = gameBoard.getBoard();
     for (let i = 0; i < board.length; i += 3) {
       console.log(`${board[i]} ${board[i + 1]} ${board[i + 2]}`);
     }
-    console.log(`Next turn: ${player1.isNext ? player1.name : player2.name}`);
+    console.log(`Next turn: ${xIsNext ? player1.name : player2.name}`);
   };
 
   const putMark = (position) => {
     const board = gameBoard.getBoard();
-    let mark = "0";
-    if (player1.isNext) {
-      mark = player1.marker;
-      player1.switchTurn();
-    } else {
-      mark = player2.marker;
-      player2.switchTurn();
-    }
 
-    board[position] = mark;
+    board[position] = xIsNext ? player1.marker : player2.marker;
+    xIsNext = !xIsNext;
 
     gameBoard.setBoard(board);
     renderBoard();
